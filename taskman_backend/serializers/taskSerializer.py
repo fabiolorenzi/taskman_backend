f577d2e31c1095c47d7447e8ee4f34a5e304c345
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-10 15:04:03 +0000

    developed task serializer

---
+++ taskman_backend/serializers/taskSerializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from ..models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            "id",
+            "number",
+            "title",
+            "description",
+            "type",
+            "priority",
+            "status",
+            "project",
+            "user",
+            "iteration",
+            "created_at",
+            "updated_at",
+            "updated_by"
+        )
