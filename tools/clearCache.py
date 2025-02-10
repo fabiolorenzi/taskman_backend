dce023d4c40d740b016c3f3145e65c33fc702719
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-10 10:15:18 +0000

    developed tool to clear the cache

---
+++ tools/clearCache.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+if os.path.isdir("__pycache__"):
+    shutil.rmtree(os.path.join("__pycache__"))
+
+if os.path.isdir("taskman_backend/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/__pycache__"))
+
+if os.path.isdir("taskman_backend/migrations/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/migrations/__pycache__"))
+
+if os.path.isdir("taskman_backend/models/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/models/__pycache__"))
+
+if os.path.isdir("taskman_backend/serializers/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/serializers/__pycache__"))
+
+if os.path.isdir("taskman_backend/static/media/data/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/static/media/data/__pycache__"))
+
+if os.path.isdir("taskman_backend/views/api/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/views/api/__pycache__"))
+
+if os.path.isdir("taskman_backend/views/routes/__pycache__"):
+    shutil.rmtree(os.path.join("taskman_backend/views/routes/__pycache__"))
+
+if os.path.isdir("tools/__pycache__"):
+    shutil.rmtree(os.path.join("tools/__pycache__"))
