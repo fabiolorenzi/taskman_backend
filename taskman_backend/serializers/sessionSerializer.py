0b25112c51ca9f7589da8af851b9b01da39e770d
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-10 14:54:20 +0000

    developed session serializer

---
+++ taskman_backend/serializers/sessionSerializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from ..models import Session
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = (
+            "id",
+            "user",
+            "passcode",
+            "expire"
+        )
