1fa64dd5b4fb7b9b4f0df329c5c2922af7fd16eb
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-10 14:51:58 +0000

    developed project serializer

---
+++ taskman_backend/serializers/projectSerializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from ..models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Project
+        fields = (
+            "id",
+            "name",
+            "description",
+            "main_user",
+            "created_at",
+            "updated_at"
+        )
