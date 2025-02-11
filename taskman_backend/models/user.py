5a779825c525a9e4cf1498c2665caa4e21ef2da6
fabiolorenzi <fabiolorenzi13975@gmail.com>
2025-02-11 15:04:41 +0000

    fixed double email issue

--- taskman_backend/models/user.py
+++ taskman_backend/models/user.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(null = False, max_length = 20)
     surname = models.CharField(null = False, max_length = 20)
-    email = models.EmailField(null = False)
+    email = models.EmailField(null = False, unique = True)
     password = models.CharField(null = False, max_length = 99)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
